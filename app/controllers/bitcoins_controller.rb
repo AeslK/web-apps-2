757d4cec8dc999995a36f36e4391ad88a80a98cc
Aesl Kim <aesl.kim@kellogg.northwestern.edu>
2023-05-11 00:39:07 +0000

    first commit

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -1,6 +1,21 @@
 class BitcoinsController < ApplicationController
 
   def index
+    bitcoin = params["amount"] # user-generated data
+
+    # connect to the Coindesk API
+    # DON'T CHANGE THIS CODE
+    # ----------------------
+    require "net/http"
+    require "json"
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    bitcoin_data = JSON.parse(response)
+    # ----------------------
+    @usd_rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+    @usd_value = @usd_rate * bitcoin.to_f
+
     # add backend code
     # render bitcoins/index view
   end
